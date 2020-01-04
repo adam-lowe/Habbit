80ccb445fe17a48bdc9715a6df3413ba04345ed7
Thomas <thomasthekingmaker@gmail.com>
2020-01-04 14:14:23 -0500

    merged utils with lib/Appjs

--- client/src/lib/API.js
+++ client/src/lib/API.js
@@ -27,5 +27,49 @@ export default {
         }
       });
     }
+  },
+
+  Todos: {
+    // Gets all todo
+    getAll: function () {
+      return axios.get("/api/todos");
+    },
+    // Gets the book with the given id
+    getOne: function (id) {
+      return axios.get("/api/todos/" + id);
+    },
+    // Deletes the book with the given id
+    deleteOne: function (id) {
+      return axios.delete("/api/todos/" + id);
+    },
+    // Saves a book to the database
+    createOne: function () {
+      return axios.post("/api/todos");
+    },
+    updateOne: function (id) {
+      return axios.put("/api/todos");
+    }
+  },
+
+  Pets: {
+    // Gets all pet
+    getAll: function () {
+      return axios.get("/api/pets");
+    },
+    // Gets the book with the given id
+    getOne: function (id) {
+      return axios.get("/api/pets/" + id);
+    },
+    // Deletes the book with the given id
+    deleteOne: function (id) {
+      return axios.delete("/api/pets/" + id);
+    },
+    // Saves a book to the database
+    createOne: function () {
+      return axios.post("/api/pets");
+    },
+    updateOne: function (id) {
+      return axios.put("/api/pets");
+    }
   }
 }
