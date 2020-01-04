12b029cbb3ca09ad3a3256de0a44e039ed150479
Thomas Beaman <thomasthekingmaker@gmail.com>
2020-01-04 16:32:29 -0500

    set the controllers, hooked tables into user, auth needs Jeffs advice

--- server/controllers/api/todosController.js
+++ server/controllers/api/todosController.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
-const db = require("../../models");
+const Todo = require("../../models/todo");
+const Users = require("../../models/user");
 
 router.get("/", (req, res) => {
-  db.Todo
+  Todo
     .find()
     .sort({ date: -1 })
     .then(dbModel => res.json(dbModel))
@@ -10,28 +11,41 @@ router.get("/", (req, res) => {
 });
 
 router.get("/:id", (req, res) => {
-  db.Todo
+  Todo
     .findById(req.params.id)
     .then(dbModel => res.json(dbModel))
     .catch(err => res.status(422).json(err));
 });
 
 router.post("/", (req, res) => {
-  db.Todo
-    .create(req.body)
+  const todoObj = {
+    title: req.body.title,
+    due: req.body.due,
+    description: req.body.description,
+  }
+  Todo
+    .create(todoObj, (err, newTodo) => {
+      if (err) {console.log(err)}
+      Users.findById(req.body.userID)
+      .then(foundUser => {
+        foundUser.todo.push(newTodo)
+        foundUser.save()
+        res.json({newTodo, foundUser})
+      })
+    })
     .then(dbModel => res.json(dbModel))
-    .catch(err => res.status(422).json(err));
+    // .catch(err => res.status(422).json(err));
 });
 
 router.put("/:id", (req, res) => {
-  db.Todo
+  Todo
     .findOneAndUpdate({ _id: req.params.id }, req.body)
     .then(dbModel => res.json(dbModel))
     .catch(err => res.status(422).json(err));
 });
 
 router.delete("/:id", (req, res) => {
-  db.Todo
+  Todo
     .findById({ _id: req.params.id })
     .then(dbModel => dbModel.remove())
     .then(dbModel => res.json(dbModel))
