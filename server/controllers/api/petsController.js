2d5a4cb1ee23021696f07697d7600c826efbd9e0
Thomas <thomasthekingmaker@gmail.com>
2020-01-04 15:15:25 -0500

    testing pet make

--- server/controllers/api/petsController.js
+++ server/controllers/api/petsController.js
@@ -1,37 +1,57 @@
 const router = require("express").Router();
-const db = require("../../models");
+const Pet = require("../../models/pet");
+const Users = require("../../models/user");
 
 router.get("/", (req, res) => {
-  db.Pet
+  Pet
     .find()
     .sort({ date: -1 })
-    .then(dbModel => res.json(dbModel))
+    .then(foundPets => res.json(foundPets))
     .catch(err => res.status(422).json(err));
 });
 
 router.get("/:id", (req, res) => {
-  db.Pet
+  Pet
     .findById(req.params.id)
-    .then(dbModel => res.json(dbModel))
+    .then(foundPet => res.json(foundPet))
     .catch(err => res.status(422).json(err));
 });
 
 router.post("/", (req, res) => {
-  db.Pet
-    .create(req.body)
-    .then(dbModel => res.json(dbModel))
-    .catch(err => res.status(422).json(err));
+  console.log("BODY", req.body)
+  console.log("Current", req.body.userID)
+  const petObj = {
+    name: req.body.name,
+    health: req.body.health
+  }
+  Pet
+    .create(petObj, (err, newPet) => {
+      if (err) {
+        console.log(err)
+        return res.status(422)
+      }
+      console.log("PET", newPet)
+      // Users.findById(req.body.userID)
+      // .then(foundUser => {
+      //   console.log("FoundUser", foundUser )
+      //   foundUser.pet.id = newPet._id
+      //   foundUser.save()
+        // res.json({newPet, foundUser})
+      // })
+      res.json(newPet)
+    })
+    // .catch(err => res.status(422).json(err));
 });
 
 router.put("/:id", (req, res) => {
-  db.Pet
+  Pet
     .findOneAndUpdate({ _id: req.params.id }, req.body)
     .then(dbModel => res.json(dbModel))
     .catch(err => res.status(422).json(err));
 });
 
 router.delete("/:id", (req, res) => {
-  db.Pet
+  Pet
     .findById({ _id: req.params.id })
     .then(dbModel => dbModel.remove())
     .then(dbModel => res.json(dbModel))
--- server/models/pet.js
+++ server/models/pet.js
@@ -3,10 +3,7 @@ const Schema = mongoose.Schema;
 
 const petSchema = new Schema({
     name: { type: String, required: true },
-    hunger: { type: Number, required: true },
-    health: { type: Number, required: true },
-    boredom: { type: Number, required: true },
-    age: { type: Number, required: true }
+    health: { type: Number, required: true }
 })
 
 const Pet = mongoose.model("Pet", petSchema);
