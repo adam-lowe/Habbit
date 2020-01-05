96dad1423433a71778d53ece8e81498b4f1a5b2e
Christie Burt <cmburt@yahoo.com>
2020-01-05 02:39:10 -0500

    redoing login page to use materializeUI and hooks

---
+++ client/src/components/Navigation/Header.js
@@ -0,0 +1,146 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+
+import PetsTwoToneIcon from '@material-ui/icons/PetsTwoTone';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+
+import AuthContext from '../../contexts/AuthContext';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+    list: {
+        width: 250,
+    },
+}));
+
+
+
+export default function MenuAppBar() {
+    const classes = useStyles();
+    const [auth, setAuth] = React.useState(true);
+    const [left, setLeft] = React.useState(false);
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const { user } = React.useContext(AuthContext);
+
+    const open = Boolean(anchorEl);
+
+    const handleChange = event => {
+        setAuth(event.target.checked);
+    };
+
+    const handleMenu = event => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+
+
+    const toggleDrawer = (open) => event => {
+        if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+            return;
+        }
+        setLeft(open);
+    };
+
+    const menuList = () => (
+        <div
+            className={classes.list}
+            role="presentation"
+            onClick={toggleDrawer(false)}
+            onKeyDown={toggleDrawer(false)}
+        >
+            <List>
+                {['Dashboard', 'Create Task', 'My Pet'].map((text, index) => (
+                    <ListItem button key={text}>
+                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+            </List>
+
+        </div>
+    );
+
+    return (
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={toggleDrawer(true)}>
+                        <PetsTwoToneIcon fontSize="large" />
+                    </IconButton>
+                    <SwipeableDrawer
+                        open={left}
+                        onClose={toggleDrawer(false)}
+                        onOpen={toggleDrawer(true)}
+                    >
+                        {menuList()}
+                    </SwipeableDrawer>
+                    <Typography variant="h3" className={classes.title}>
+                        Habbit
+          </Typography>
+                    {auth && (
+                        <div>
+                            <IconButton
+                                aria-label="account of current user"
+                                aria-controls="menu-appbar"
+                                aria-haspopup="true"
+                                onClick={handleMenu}
+                                color="inherit"
+                            >
+                                <AccountCircle fontSize="large" />
+                            </IconButton>
+                            <Menu
+                                id="menu-appbar"
+                                anchorEl={anchorEl}
+                                anchorOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                open={open}
+                                onClose={handleClose}
+                            >
+                                <MenuItem>{user.name}</MenuItem>
+                                <MenuItem onClick={handleChange}>Logout</MenuItem>
+                            </Menu>
+                        </div>
+                    )}
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+}
