4e6be58c212d37361a0847ba35e9542bee96f349
Goldpm <lowedavidadam@gmail.com>
2020-01-07 00:56:03 -0500

    converted PetStatus for materialUI

--- client/src/components/PetStatus/PetStatus.js
+++ client/src/components/PetStatus/PetStatus.js
@@ -1,34 +1,44 @@
-import React, { Component } from 'react';
+import React from "react";
 import { Line } from 'rc-progress';
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import Button from "@material-ui/core/Button";
 
-class PetStatus extends Component {
-    state = {
+const useStyles = makeStyles(theme => ({
+    button: {
+        margin: 10
+    },
+    card: {
+        minWidth: 275,
+        margin: 20
+    },
+    img: {
+        margin: 20
+    },
+    line: {
+        margin: 15
+    }
+}));
+
+export default function PetStatus(props) {
+    const classes = useStyles();
+    const [state, setState] = React.useState({
         health: 100
-    };
-
-    loseHealth = () => {
-        const pHealth = this.state.health - 10
-        this.setState({
+    });
+    const loseHealth = () => {
+        const pHealth = state.health - 10
+        setState({
             health: pHealth
         });
     }
-
-    render() {
-        const { health } = this.state;
-
-        return (
-            <div className='PetStatus'>
-                <div className='card'>
-                    <div className='card-body'>
-                    <img src="http://placekitten.com/400/300" alt="Kitten"/>
-                    <Line percent={ health } strokeWidth="2" strokeColor="#2db7f5" />
-                    <button className='btn btn-primary' onClick={this.loseHealth} type='submit'>Pass Time</button>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+    return (
+        <div className='PetStatus'>
+            <Card className={classes.card}>
+                    <img className={classes.img} src="http://placekitten.com/400/300" alt="Kitten" />
+                    <Line className={classes.line} percent={state.health} strokeWidth="2" strokeColor="#2db7f5" />
+                    <Button className={classes.button} type="submit" size="large" onClick={loseHealth} variant="outlined" color="primary">Pass Time</Button>
+            </Card>
+        </div>
+    )
 }
-
-export default PetStatus;
 
