a08df64dec516f9ff80bd0cf1d93f4be76242373
Goldpm <lowedavidadam@gmail.com>
2020-01-07 02:04:40 -0500

    Converted Pet Status component to MaterialUI

--- client/src/components/PetStatus/PetStatus.js
+++ client/src/components/PetStatus/PetStatus.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Line } from 'rc-progress';
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles(theme => ({
@@ -34,9 +35,11 @@ export default function PetStatus(props) {
     return (
         <div className='PetStatus'>
             <Card className={classes.card}>
+                <CardContent>
                     <img className={classes.img} src="http://placekitten.com/400/300" alt="Kitten" />
                     <Line className={classes.line} percent={state.health} strokeWidth="2" strokeColor="#2db7f5" />
                     <Button className={classes.button} type="submit" size="large" onClick={loseHealth} variant="outlined" color="primary">Pass Time</Button>
+                </CardContent>
             </Card>
         </div>
     )
