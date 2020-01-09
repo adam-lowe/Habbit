4b80dc52070edd96ee09eb2420c591e8acbddcf1
Thomas Beaman <thomasthekingmaker@gmail.com>
2020-01-09 14:13:06 -0500

    font changes, added habbit pic

--- client/src/components/PetStatus/PetStatus.js
+++ client/src/components/PetStatus/PetStatus.js
@@ -4,7 +4,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
-
+import Habbit from "../Assets/TheHabbit.png"
 const useStyles = makeStyles(theme => ({
     button: {
         margin: 10
@@ -14,7 +14,9 @@ const useStyles = makeStyles(theme => ({
         margin: 20
     },
     img: {
-        margin: 20
+        margin: 20,
+        height: 400,
+        width: 525
     },
     line: {
         margin: 15
@@ -27,8 +29,8 @@ export default function PetStatus(props) {
         <div className='PetStatus'>
             <Card className={classes.card}>
                 <CardContent>
-                    <img className={classes.img} src={`http://placekitten.com/200/200`} alt="Kitten" />
-                    <Line className={classes.line} percent={props.health} strokeWidth="7" trailColor="#000000" strokeLinecap="square" strokeColor="#008000" />
+                    <img className={classes.img} src={Habbit} alt="Kitten" />
+                    <Line className={classes.line} percent={props.health} strokeWidth="5" trailColor="#000000" strokeLinecap="square" strokeColor="#008000" />
                 </CardContent>
             </Card>
         </div>
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-
+import './fonts/HelloCute.ttf'
 import './index.css';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
