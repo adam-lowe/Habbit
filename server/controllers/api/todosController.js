bbc1eb6a6c9f33dd1c5aa9bff9058947a9af66d3
Christie Burt <cmburt@yahoo.com>
2020-01-09 23:00:37 -0500

    I was taken to task by a task list

--- server/controllers/api/todosController.js
+++ server/controllers/api/todosController.js
@@ -1,53 +1,40 @@
 const router = require("express").Router();
 const Todo = require("../../models/todo");
 const Users = require("../../models/user");
-const { JWTVerifier } = require('../../lib/passport');
+const { JWTVerifier } = require("../../lib/passport");
 
 router.get("/", JWTVerifier, (req, res) => {
-  Users
-    .findById(req.user._id)
+  Users.findById(req.user._id)
     .then(user => res.json(user.todos))
     .catch(err => res.status(500).json(err));
 });
 
 router.get("/:id", JWTVerifier, (req, res) => {
-  Todo
-    .findById(req.params.id)
+  Todo.findById(req.params.id)
     .then(dbModel => res.json(dbModel))
     .catch(err => res.status(422).json(err));
 });
 
 router.post("/", JWTVerifier, (req, res) => {
-  const todoObj = {
-    title: req.body.title,
-    due: req.body.due,
-    description: req.body.description,
-  }
-  Todo
-    .create(todoObj, JWTVerifier, (err, newTodo) => {
-      if (err) {console.log(err)}
-      Users.findById(req.body.userID)
-      .then(foundUser => {
-        foundUser.todo.push(newTodo)
-        foundUser.save()
-        res.json({newTodo, foundUser})
-      })
+  const todos = req.user.todos;
+  const newTodo = req.body;
+  newTodo.id = todos.length + 1;
+  Users.updateOne({ _id: req.user._id }, req.user)
+    .then(user => {
+      delete user.password;
+      res.json(user);
     })
-    // .then(dbModel => res.json(dbModel))
-    // .catch(err => res.status(422).json(err));
+    .catch(err => res.status(500).json(err));
 });
 
 router.put("/:id", JWTVerifier, (req, res) => {
-  Todo
-    .findOneAndUpdate({ _id: req.params.id }, req.body)
+  Todo.findOneAndUpdate({ _id: req.params.id }, req.body)
     .then(dbModel => res.json(dbModel))
     .catch(err => res.status(422).json(err));
 });
 
 router.delete("/:id", JWTVerifier, (req, res) => {
-
-  Todo
-    .findById({ _id: req.params.id })
+  Todo.findById({ _id: req.params.id })
     .then(dbModel => dbModel.remove())
     .then(dbModel => res.json(dbModel))
     .catch(err => res.status(422).json(err));
